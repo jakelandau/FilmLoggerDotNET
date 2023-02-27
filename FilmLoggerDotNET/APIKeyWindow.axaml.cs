4fb482a83386c8b541839cd73e5330b53984c658
Jake Landau <5353216+jakelandau@users.noreply.github.com>
2023-02-27 07:17:59 -0500

    Fully documented all functions to prepare for release

--- FilmLoggerDotNET/APIKeyWindow.axaml.cs
+++ FilmLoggerDotNET/APIKeyWindow.axaml.cs
@@ -21,10 +21,15 @@ namespace FilmLoggerDotNET
 
         private async void OkButtonClick(object? sender, RoutedEventArgs e)
         {
+            // Adds TMDb API Key from text input to dictionary
             Dictionary<string,string> apiKey = new Dictionary<string,string>();
             apiKey.Add("TMDbAPI", TMDbAPIKey.Text);
+
+            // Serializes API Key into secret.json
             string json = JsonConvert.SerializeObject(apiKey, Formatting.Indented);
             await File.WriteAllTextAsync("secret.json", json);
+
+            // Closes Window
             Close();
         }
     }
--- FilmLoggerDotNET/MainWindow.axaml.cs
+++ FilmLoggerDotNET/MainWindow.axaml.cs
@@ -19,8 +19,8 @@ namespace FilmLoggerDotNET
         private List<Film> workingMovieArchive = new List<Film>(); // Working archive of all movies user has watched
         private List<Film> safetyCheckMovieArchive = new List<Film>(); // Copy of above made for safety check; if working archive doesn't match safety check, data has not been saved
 
-        private Dictionary<string, string> APIKeys;
-        private TMDbClient webClient;
+        private Dictionary<string, string> APIKeys; // Dictionary of API Keys, loaded in and out of secret.json
+        private TMDbClient webClient; // Client for accessing TheMovieDB API
 
         private Film currentMovie = new Film(); // Film object in buffer to add to working archive
         private bool isVerified = false; // Latch to ensure IMDb ID has been verified before attempting to add to archive
@@ -39,7 +39,7 @@ namespace FilmLoggerDotNET
             DateSeen.MaxYear = DateTimeOffset.Now;
             DateSeen.MinYear = DateTimeOffset.UnixEpoch;
 
-            // Associates Buttons with Click EventHandler functions
+            // Associates Buttons with event handlers
             Button FilePickerButton = this.FindControl<Button>("FilePicker");
             FilePickerButton.Click += FilePickerButtonClick;
 
@@ -52,12 +52,16 @@ namespace FilmLoggerDotNET
             Button AddMovieButton = this.FindControl<Button>("AddMovie");
             AddMovieButton.Click += AddMovieButtonClick;
 
+            // Associates Menu items with event handlers
             DumpFileMenu.Click += DumpFileButtonClick;
             OpenFileMenu.Click += FilePickerButtonClick;
             APIKeyMenu.Click += GetAPIKey;
+            LicenseMenu.Click += ShowFilmLoggerLicense;
 
+            // Ensures user has chance to save before closing application
             Closing += MainWindowClosing;
         }
+
 
         private async void FilePickerButtonClick(object? sender, RoutedEventArgs e)
         {
@@ -115,13 +119,11 @@ namespace FilmLoggerDotNET
             }
         }
 
-        private async void DumpFileButtonClick(object? sender, RoutedEventArgs e)
-        {
-            SaveFile();
-        }
+        private void DumpFileButtonClick(object? sender, RoutedEventArgs e) => SaveFile();
 
         private async void VerifyButtonClick(object? sender, RoutedEventArgs e)
         {
+            // Checks if API Key is stored on disk, if not calls GetAPIKey
             if (File.Exists("secret.json"))
             {
                 APIKeys = JsonConvert.DeserializeObject<Dictionary<string, string>>(File.ReadAllText("secret.json"));
@@ -131,10 +133,15 @@ namespace FilmLoggerDotNET
                 GetAPIKey(sender, e);
             }
 
+
             try
             {
+                // Instances the TMDb client with the stored API Key
                 webClient = new TMDbClient(APIKeys["TMDbAPI"]);
 
+                // Attempts to verify film with TheMovieDB backend,
+                // if successful populates initial data into currentMovie buffer
+                // and releases verification latch
                 Movie webSourcedFilm = await webClient.GetMovieAsync(imdbId: IMDbID.Text);
                 currentMovie = new Film
                 {
@@ -143,8 +150,10 @@ namespace FilmLoggerDotNET
                 };
                 isVerified = true;
 
+                // Sets poster image to the TMDb link provided by the API
                 PosterImage.Source = $"https://image.tmdb.org/t/p/w500/{webSourcedFilm.PosterPath}";
 
+                // Informs user their film is ready to add to archive
                 var successBox = MessageBoxManager.GetMessageBoxStandardWindow(new MessageBoxStandardParams
                 {
                     ButtonDefinitions = MessageBox.Avalonia.Enums.ButtonEnum.Ok,
@@ -161,11 +170,14 @@ namespace FilmLoggerDotNET
             }
             catch
             {
+                // Clears buffers and re-sets verification latch
                 currentMovie = new Film();
                 isVerified = false;
 
+                // Sets poster back to blank
                 PosterImage.Source = "./Assets/blank_poster.jpg";
 
+                // Informs user of failure and provides troubleshooting steps
                 var errorBox = MessageBoxManager.GetMessageBoxCustomWindow(new MessageBoxCustomParamsWithImage
                 {
                     ButtonDefinitions = new[]
@@ -243,6 +255,7 @@ namespace FilmLoggerDotNET
                     await errorBox.ShowDialog(this);
                 }
             }
+            // Informs user the verification latch has not been released
             else
             {
                 var errorBox = MessageBoxManager.GetMessageBoxStandardWindow(new MessageBoxStandardParams
@@ -263,10 +276,13 @@ namespace FilmLoggerDotNET
 
         private async void MainWindowClosing(object sender, CancelEventArgs e)
         {
+            // Checks if unsaved film watching data is present
             if (workingMovieArchive.Count > safetyCheckMovieArchive.Count)
             {
+                // Cancels window close event
                 e.Cancel = true;
 
+                // Creates dialog box asking if the user wants to save changes
                 var saveBox = MessageBoxManager.GetMessageBoxCustomWindow(new MessageBoxCustomParams
                 {
                     ButtonDefinitions = new[] {
@@ -281,8 +297,12 @@ namespace FilmLoggerDotNET
                     WindowStartupLocation = WindowStartupLocation.CenterScreen,
                     WindowIcon = new WindowIcon(iconPath)
                 });
-
                 var userSelection = await saveBox.ShowDialog(this);
+
+                /* If the user picks yes, saves the file and closes. If the
+                 user picks no, sets the working archive equal to the safety
+                 archive and closes. Since the counts are equal, the closing
+                 process continues without interrupt */
                 if (userSelection == "Save archive")
                 {
                     SaveFile();
@@ -340,6 +360,7 @@ namespace FilmLoggerDotNET
                     // PASS
                 }
             }
+            // Bypasses file dump if no new data has been added
             else if (workingMovieArchive.Count == safetyCheckMovieArchive.Count)
             {
                 var errorBox = MessageBoxManager.GetMessageBoxStandardWindow(new MessageBoxStandardParams
@@ -360,15 +381,34 @@ namespace FilmLoggerDotNET
 
         private void UpdateFilmCount()
         {
+            // Updates visual film count indicator on screen
             int count = workingMovieArchive.Count;
             ArchiveCount.Text = $"Film Count: {count}";
         }
 
         private async void GetAPIKey(object? sender, RoutedEventArgs e)
         {
+            // Opens APIKeyWindow to retrieve TheMovieDB API Key from user
             var APIKeyDialog = new APIKeyWindow();
             APIKeyDialog.WindowStartupLocation = WindowStartupLocation.CenterScreen;
             await APIKeyDialog.ShowDialog(this);
+        }
+
+        private async void ShowFilmLoggerLicense(object? sender, RoutedEventArgs e)
+        {
+            // Displays software license for FilmLogger, indicating the program
+            // is licensed under the terms of AGPLv3-only.
+            var licenseBox = MessageBoxManager.GetMessageBoxStandardWindow(new MessageBoxStandardParams
+            {
+                ButtonDefinitions = MessageBox.Avalonia.Enums.ButtonEnum.Ok,
+                ContentTitle = "FilmLogger License",
+                ContentMessage = "Copyright © 2023 Jake Landau\r\n\r\nThis program is free software; you can redistribute it and/or modify it under the terms of the GNU Affero General Public License\r\n\r\nas published by the Free Software Foundation, specifically version 3.\r\n\r\nThis program is distributed in the hope that it will be useful,but WITHOUT ANY WARRANTY; without even the implied warranty of\r\n\r\nMERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License, version 3 for more details.\r\n\r\nA copy of the license is attached in the file `LICENSE.MD`.",
+                Markdown = true,
+                WindowStartupLocation = WindowStartupLocation.CenterScreen,
+                SizeToContent = SizeToContent.WidthAndHeight,
+                WindowIcon = new WindowIcon(iconPath)
+            });
+            await licenseBox.ShowDialog(this);
         }
     }
 }
